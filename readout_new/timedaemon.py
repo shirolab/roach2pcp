8c289efdbd75a6f8231407dab5fda85a139cf4fd
kkarkare <kkarkare@kicp.uchicago.edu>
2018-02-24 14:50:30 -0600

    write current ctime to one ROACH, need to add interface to config files

---
+++ readout_new/timedaemon.py
@@ -0,0 +1,24 @@
+# World's simplest python daemon to write current ctime to ROACH
+# register, based on https://stackoverflow.com/a/8375012
+# Run with $ python timedaemon.py
+# Need to make this interface with a config file to choose the
+# right ROACH when there is more than one
+
+import daemon
+import time
+import kidPy
+
+def write_ctime(fpga):
+    while True:
+        fpga.write_int('GbE_ctime',time.time())
+
+def run():
+    with daemon.DaemonContext():
+        fpga = kidPy.getFPGA()
+        write_ctime(fpga)
+
+if __name__ == "__main__":
+    run()
+
+
+    
