65e5a41082a2e5c438b3636428d46908dffdfa0f
kkarkare <kkarkare@kicp.uchicago.edu>
2018-02-26 16:34:26 -0600

    now contains absolute minimum to get correctly-formatted packets out of ROACH

--- readout_new/main.py
+++ readout_new/main.py
@@ -56,7 +56,11 @@ spacing_threshold  = np.float(gc[np.where(gc == 'spacing_threshold')[0][0]][1])
 
 ###################################################
 # Minimal set of things to initialize
-fpga = kp.getFPGA()
+# taken from kidPy/main
+
+s = None
+fpga = casperfpga.katcp_fpga.KatcpFpga(gc[np.where(gc == 'roach_ppc_ip')[0][0]][1], timeout = 120.)
+#fpga = kp.getFPGA()
 
 # UDP socket
 s = socket(AF_PACKET, SOCK_RAW, htons(3))
@@ -64,14 +68,66 @@ s = socket(AF_PACKET, SOCK_RAW, htons(3))
 #s.bind((gc[np.where(gc=='udp_dest_device')[0][0]][1],3))
 
 # Valon synthesizer instance
-valon = kp.getValon()
+valon = valon_synth9.Synthesizer(gc[np.where(gc == 'valon_comm_port')[0][0]][1])
+#valon = kp.getValon()
     
 # Roach interface
 ri = roachInterface(fpga, gc, regs, valon)
 
 # GbE interface
 udp = roachDownlink(ri, fpga, gc, regs, s, ri.accum_freq)
-udp.configSocket() # already configured up top
+udp.configSocket()
+
+# At this point in the main script we go to main_opt
+
+# 0: Test connection to ROACH
+result = kp.testConn(fpga)
+
+# 1: Upload firmware
+ri.uploadfpg()
+
+# 2: Initialize system & UDP conn
+kp.initValon(valon)
+fpga.write_int(regs[np.where(regs == 'accum_len_reg')[0][0]][1], ri.accum_len - 1) # VERY necessary
+#time.sleep(0.1)
+#ri.qdrCal() # not necessary
+#fpga.write_int(regs[np.where(regs == 'write_qdr_status_reg')[0][0]][1], 1) # not necessary...
+udp.configDownlink()
+
+# 3: Write test comb
+ri.makeFreqComb()
+#fpga.write_int(regs[np.where(regs == 'fft_shift_reg')[0][0]][1], 2**5 -1) # not necessary...
+time.sleep(0.1)
+ri.upconvert = np.sort(((ri.freq_comb + (center_freq)*1.0e6))/1.0e6)
+#print "RF tones =", ri.upconvert
+ri.writeQDR(ri.freq_comb, transfunc = False) # SUPER necessary
+#np.save("last_freq_comb.npy", ri.freq_comb)
+""" None of this shit needed!!!
+if not (fpga.read_int(regs[np.where(regs == 'dds_shift_reg')[0][0]][1])):
+    if regs[np.where(regs == 'DDC_mixerout_bram_reg')[0][0]][1] in fpga.listdev():
+        shift = ri.return_shift(0)
+        if (shift < 0):
+            print "\nError finding dds shift: Try writing full frequency comb"
+            
+        else:
+            fpga.write_int(regs[np.where(regs == 'dds_shift_reg')[0][0]][1], shift)
+            print "Wrote DDS shift (" + str(shift) + ")"
+    else:
+        fpga.write_int(regs[np.where(regs == 'dds_shift_reg')[0][0]][1], ri.dds_shift)
+"""        
+# 7: Get system state
+kp.getSystemState(fpga, ri, udp, valon)
+
+# 8: Test downlink
+udp.testDownlink(5)
+# if ok (returns 0), write to this reg...
+fpga.write_int(regs[np.where(regs == 'write_stream_status_reg')[0][0]][1], 1)
+
+"""
+# 9: Stream 
+time_interval = 0.1 # or whatever
+chan = 0 # or whatever
+udp.printChanInfo(chan, time_interval)
 
 ######################################################
 # To read out a packet:
@@ -87,7 +143,7 @@ packet, data, header, saddr = udp.parsePacketData()
 while True:
     fpga.write_int('GbE_ctime',time.time())
 
-
+"""
 
 
 
