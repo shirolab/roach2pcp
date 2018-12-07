33e7df38a0beea77c0971148f25b5222ae5f6840
sr-cdf <rowes@cardiff.ac.uk>
2018-12-07 14:48:01 +0000

    added dummy synth modelnum = "dummy"

--- pcp/synthesizer/__init__.py
+++ pcp/synthesizer/__init__.py
@@ -12,7 +12,8 @@ _cwd = _os.path.dirname(__file__)
 _modlist = [ _os.path.join(_cwd, _f) for _f in _os.listdir(_cwd) if _os.path.splitext(_f)[-1] == ".py" ]
 __all__ = [ _os.path.basename(_f)[:-3] for _f in _modlist if _os.path.isfile(_f) and not _f.endswith('__init__.py')]
 
-print _modlist
+#print _modlist
+
 # import all modules found by __all__
 
 # create a dictionary of available synth modules that will be used by lib_hardware
@@ -23,4 +24,4 @@ for classname, synthobj in _inspect.getmembers(synthclasses, _inspect.isclass):
     for modelnum in getattr(synthobj, "MODELNUMS"):
         # join vendor and model numbers (all lower case)
         SYNTH_HW_DICT[ "_".join( (getattr(synthobj, "VENDOR"), str(modelnum).lower() ) )] = synthobj
-print 'synthesizer:__init__.py SYNTH_HW_DICT:', SYNTH_HW_DICT
+#print 'synthesizer:__init__.py SYNTH_HW_DICT:', SYNTH_HW_DICT
