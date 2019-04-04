2db99d80e62bb704e39b3757550d6af58f34717f
pete <petebarry1988@gmail.com>
2019-04-04 22:37:04 +0000

    general-purpose equations, automated auxiliary and magnitude plotting

---
+++ pcp/kst/scripts.py
@@ -0,0 +1,33 @@
+# Scripts to generate useful plots for kst monitoring
+# Uses lower-level functions in ssMonitor.py
+
+import ssMonitor
+
+def plot_aux_data():
+    df = ssMonitor.find_latestfile()
+    client_name = 'aux_data'
+
+    field_list = ['packet_count','pps_timestamp','fine_timestamp',
+                  'roach_checksum','packet_info_reg']
+
+    for fn in field_list:
+        client = ssMonitor.plot_dirfile_rawfield(fn,
+                                                 client_name = client_name,
+                                                 datafile = df)
+    return client
+
+
+def plot_kidrange_mag(kidarray): # array of numbers
+    df = ssMonitor.find_latestfile()
+    client_name = 'magnitude'
+
+    chanlist = []
+    for kk, kn in enumerate(kidarray):
+        chanlist.append('K%03i' % kn)
+        
+    client = ssMonitor.plot_dirfile_mag(chanlist,
+                                        client_name = client_name,
+                                        datafile = df)
+
+    return client
+    
