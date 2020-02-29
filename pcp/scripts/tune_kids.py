abb6084ea8cecf041b11ff0098078c24cb50cdb0
kkarkare <kkarkare@kicp.uchicago.edu>
2020-02-29 19:06:57 +0000

    placeholder

--- pcp/scripts/tune_kids.py
+++ pcp/scripts/tune_kids.py
@@ -1,24 +1,34 @@
 #!/usr/bin/env python
 
-# initliation script.
-# This script is the first code to be run after all hardware in connected and switched on
-# run as
+# General script to tune KIDs
+# Ideally we eventually stick most of this functionality into mux_channel
 
-# We could write a small helper script to check things are connected would be useful for initial configuration testing
-#    - check dnsmasq is running
-#   - check roach(s) are connected
-#
-import logging as _logging
 _logger = _logging.getLogger(__name__)
 
 from .. import ROACH_LIST, mux_channel
 
-def print_current_roaches(muxchannel):
+def main(muxch):
+    _logger.info( "SCRIPT: tune_kids" )
 
-    print "this is {roachid}".format(roachid = muxchannel.roachid)
+    # if we haven't already swept, do the first sweep
+    if not muxch.sweep.dirfile or force_first_sweep == True:
+        # sweep
+        muxch.sweep_lo(**swpkwargs)
 
-def verify_configuration():
-    pass
+    # remove blind tones/ don't retune blind tones
 
-def main(test):
-    _logger.info( "This is the function that is run {0}".format(test) )
+    # analyse sweep - tone_freqs = None returns parameters at that F0, and can be used to find F0s
+    muxch.sweep.calc_sweep_cal_params( tonefreqs = None, method = findf0_method )
+    muxch.sweep.write_sweep_cal_params(overwrite = True)
+    # use method to find F0s from KID rountines
+    _logger.debug( "new f0s found- {0}".format(muxch.sweep.calparams['f0s']) )
+    # change frequencies in self.toneslist
+    muxch.toneslist.bb_freqs = muxch.sweep.calparams['f0s'] - muxch.toneslist.lo_freq
+    # write to qdr (optional)
+    muxch.write_freqs_to_fpga( auto_write = True )
+    # resweep with new tones
+    muxch.sweep_lo(**swpkwargs)
+    # recalculate sweep cal params, this time with the written tones
+    muxch.sweep.calc_sweep_cal_params( tonefreqs = muxch.toneslist.rf_freqs, method = findf0_method )
+    # write these to the new sweep file
+    muxch.sweep.write_sweep_cal_params(overwrite = True)
