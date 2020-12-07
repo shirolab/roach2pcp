b7a785e290a84542e3c8337edfc6f28b41cda51a
sr-cdf <rowesamrowe@gmail.com>
2020-12-07 15:15:47 +0000

    add optional median filter to calc_sweep_params when finding max(speed)

--- pcp/sweep.py
+++ pcp/sweep.py
@@ -144,7 +144,7 @@ class pcpSweep(object):
 
 
 
-    def calc_sweep_cal_params(self, tonefreqs = None, method = "maxspeed", exclude_idxs=[], exclude_endpoints=2, choose_range=False):
+    def calc_sweep_cal_params(self, tonefreqs = None, method = "maxspeed", exclude_idxs=[], exclude_endpoints=2, choose_range=False,despike_window=None):
         """
         Function to calculate the calibration parameters from the currently loaded sweep data and filter parameters.
 
@@ -173,12 +173,10 @@ class pcpSweep(object):
                 nan_mask[:, len(self.rf_freqs[0,:])/2 :] = True # not being super careful about even/odd
             if choose_range == 'right':
                 nan_mask[:, 0:len(self.rf_freqs[0,:])/2] = True
-
-        calparams, caldata = _resonator_routines.calc_sweep_cal_params(self.rf_freqs, \
-                                                                                self.data.real, \
-                                                                                self.data.imag, \
-                                                                                tonefreqs, \
-                                                                                nanmask = nan_mask)
+        
+        calparams, caldata = _resonator_routines.calc_sweep_cal_params(
+            self.rf_freqs,self.data.real,self.data.imag, tonefreqs, nanmask =nan_mask,despike_window=despike_window)
+                                                                            
         # get the indexes of the data we want to analyse (default all of them )
         allidxs = _np.arange( len(self.data), dtype=_np.int )
         idxstoanalyse = list( set(allidxs).difference(exclude_idxs) )
